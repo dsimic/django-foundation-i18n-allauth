1eebd79a05b043efbc0dcec2d8a165de0e8ac5e2
David Simic <dpsimic@gmail.com>
2015-01-08 15:53:16 -0500

    add db sqlite3 to .gitignore

--- project_name/project_name/views.py
+++ project_name/project_name/views.py
@@ -1,7 +1,12 @@
-#coding: utf-8
+# coding: utf-8
 from annoying.decorators import render_to
 
 
 @render_to('home.html')
 def home(request):
+    return {}
+
+
+@render_to('train.html')
+def train(request):
     return {}
